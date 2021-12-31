b56e321aee70a045b07bd9e6d154ed140bba9066
Hakuen9 <zhahir.0g@gmail.com>
2021-12-31 12:57:02 +0100

    Implemented a basic Dad bot.

--- src/main/java/WeCodeStuffHere/components/DadComponent.java
+++ src/main/java/WeCodeStuffHere/components/DadComponent.java
@@ -1,9 +1,21 @@
 package WeCodeStuffHere.components;
 
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DadComponent extends Component {
+    private final List<String> triggers = new ArrayList<>() {
+        {
+            add("i am");
+            add("i'm");
+            add("my name is");
+        }
+    };
 
     @Override
     public void onReady(ReadyEvent event)
@@ -15,6 +27,20 @@ public class DadComponent extends Component {
     @Override
     public void onMessageReceived(MessageReceivedEvent event)
     {
-        System.out.printf("[%s]: %s\n", event.getAuthor().getName(), event.getMessage().getContentDisplay());
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+
+        String message = event.getMessage().getContentDisplay().toLowerCase();
+        List<String> triggersInMessage = triggers.stream()
+                .filter(message::contains)
+                .collect(Collectors.toList());
+
+        if (triggersInMessage.size() > 0) {
+            String trigger = triggersInMessage.get(0);
+            String dadJoke = message.substring(message.lastIndexOf(trigger) + trigger.length() + 1);
+            MessageChannel channel = event.getChannel();
+            channel.sendMessage("Hi " + dadJoke + ", I'm Dad!").queue();
+        }
     }
 }
